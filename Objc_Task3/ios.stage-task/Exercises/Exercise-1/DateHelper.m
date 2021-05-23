f951e11fcf4bb265a67145c9c4c22a904d144d69
rolll13 <r.mikulich@mail.ru>
2021-05-24 01:04:02 +0300

    feat:add exercise1 and exercise2 implementtation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,41 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if (0 < monthNumber && monthNumber <= 12) {
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        NSString *month = [[dateFormatter monthSymbols] objectAtIndex:monthNumber - 1];
+        return month;
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    return [[NSCalendar currentCalendar] component:NSCalendarUnitDay fromDate:[dateFormatter dateFromString:date]];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"EE";
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    NSLog(@"%@", [dateFormatter stringFromDate:date]);
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger week = [calendar component:NSCalendarUnitWeekOfYear fromDate:date];
+    NSInteger thisWeek = [calendar component:NSCalendarUnitWeekOfYear fromDate:[NSDate now]];
+    if (week == thisWeek){
+        return YES;
+    }
     return NO;
 }
 
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,24 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int output = 0;
+        BOOL iBiggerJ, iLessJ, jBiggerK, jLessK, sort, sortReverse;
+        for (int i = 0; i < ratingArray.count; ++i) {
+            for (int j = i + 1; j < ratingArray.count; ++j) {
+                for (int k = j + 1; k < ratingArray.count; ++k) {
+                    jLessK = [ratingArray[j] intValue] < [ratingArray[k] intValue];
+                    iLessJ = [ratingArray[i] intValue] < [ratingArray[j] intValue];
+                    jBiggerK = [ratingArray[j] intValue] > [ratingArray[k] intValue];
+                    iBiggerJ = [ratingArray[i] intValue] > [ratingArray[j] intValue];
+                    sort = iLessJ && jLessK;
+                    sortReverse = iBiggerJ && jBiggerK;
+                    if (sort || sortReverse){
+                        output++;
+                    }
+                }
+            }
+        }
+        return output;
 }
 
 @end
